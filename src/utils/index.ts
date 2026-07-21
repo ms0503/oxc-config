9a4782e23543de3d6d34c62e6f5020e5e4d96729
Sora Tonami <ms0503@outlook.com>
2026-07-22 01:32:46 +0900

    docs: add jsdoc to convertglobals

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,16 @@
-import type { OxlintGlobals } from 'oxlint';
+import type { OxlintGlobals } from "oxlint";
 
+/**
+ * Converts global variable definitions in 'globals' to oxlint-compatible definitions.
+ *
+ * @param inputs Global variable definitions in 'globals'
+ * @returns Oxlint-compatible global variable definitions
+ */
 export function convertGlobals(inputs: Record<string, boolean>): OxlintGlobals {
     return Object.fromEntries(
-        Object.entries(inputs).map(([k, v]) => [k, v ? 'writable' : 'readonly'])
+        Object.entries(inputs).map(([k, v]) => [
+            k,
+            v ? "writable" : "readonly",
+        ]),
     );
 }
